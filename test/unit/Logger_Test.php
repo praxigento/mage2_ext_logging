fc8691b60944a1ad0de86f66a1b926aa991ca0e4
Alex Gusev <alex@flancer64.com>
2015-11-26 19:43:32 +0200

    MOBI-128 - Create standalone M2 extension for Cascade Monolog

--- test/unit/Logger_Test.php
+++ test/unit/Logger_Test.php
@@ -2,12 +2,33 @@
 /**
  * User: Alex Gusev <alex@flancer64.com>
  */
-namespace Praxigento\Logger;
+namespace Praxigento\Logging;
+
+use Magento\TestFramework\ObjectManager;
+
 include_once(__DIR__ . '/phpunit_bootstrap.php');
 
 class Logger_UnitTest extends \PHPUnit_Framework_TestCase {
 
     public function test_constructor_empty() {
+        /** Mock ObjectManager and runtime environment */
+        $mockObm = $this
+            ->getMockBuilder('Magento\TestFramework\ObjectManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        ObjectManager::setInstance($mockObm);
+        // $this->_logger = ObjectManager::getInstance()->get('Magento\Framework\Logger\Monolog');
+        $mockDefaultLogger = $this
+            ->getMockBuilder('Magento\Framework\Logger\Monolog')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mockObm
+            ->expects($this->once())
+            ->method('get')
+            ->willReturn($mockDefaultLogger);
+        /**
+         * Perform testing.
+         */
         $cfg = new Logger();
         $this->assertTrue($cfg instanceof \Praxigento\Logging\Logger);
     }
