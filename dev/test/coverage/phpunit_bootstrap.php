e227e90cdebdea7714a3f97856459fd0e5ed2f1f
Alex Gusev <alex@flancer64.com>
2016-02-17 10:54:29 +0200

    MOBI-219 - Fix Travis tests for praxigento / mage2_ext_logging

--- dev/test/coverage/phpunit_bootstrap.php
+++ dev/test/coverage/phpunit_bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 /* Composer autoloader. */
-include_once(__DIR__ . '/../../vendor/autoload.php');
+include_once(__DIR__ . '/../../../work/vendor/autoload.php');
 if(!defined('IS_M2_TESTS')) {
     define('IS_M2_TESTS', true);
 }
